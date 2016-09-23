1bc9c9650b9ed13d4a8599b06609da42bbe86cf7
Khuyen Nguyen <khuyenvnguyen@gmail.com>
2016-09-23 07:07:04 +0000

    fix insert issue

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ var express = require('express');
 var mongo = require('mongodb').MongoClient;
 var path = require('path');
 var localdb = "mongodb://localhost:27017/mydb";
-var url_db = localdb || process.env.DB_URL;
+var url_db = process.env.DB_URI || localdb;
 var app = express();
 var resObj = {};
 var digit = 10;
-var pool = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var maxStorage = Math.pow(pool.length, digit);
+var pool = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname, 'index.html')) ;
@@ -25,22 +23,70 @@ app.get(/short\/(.+)/, function(req, res){
         return res.json(obj);
     }
 
-    
+    //console.log('variable', url_db);
     obj["original_url"] = url;
     
-        accessDb(url, function(err, col){
+    accessDb(function(err, db, col){
            if(err) throw err;
            
-           col.find().toArray(function(err, data){
+           //col.find().toArray(function(err, data){
+               //if(err) throw err;
                //console.log('showdata',data);
-           });
+           //});
+       
+          findUrl(url, function(err, data){
+             if(err) throw err;
+             if(data){
+                 obj["short_url"] = baseUrl+ data;
+                 db.close();
+                 res.json(obj);
+             }else{ 
+                findKey(function(key){
+                    console.log('final key', key);
+                    obj["short_url"] = baseUrl + key;
+                    col.insert({'key': key, 'url':url});
+                    db.close();
+                    res.json(obj);
+                });
+             }
+                 
+          });
+              
+            function findUrl(searchUrl, callback){
+                
+              col.findOne({'url': searchUrl}, function(err, data){
+                  if(!data){
+                    callback(err, null);
+                  }else {
+                    console.log('thisdata_part',data)  
+                    //obj["short_url"] = baseUrl + data[0]['key'];
+                    callback(err, data['key']);
+                  }
+              });
+                  
+            }
+              
+            function findKey(callback){
+              
+              var newKey = codeGen(digit, "");
+              col.findOne({key: newKey}, function(err, data){
+                  if(err) throw err;
+                  //if(!data) return findKey(callback);
+                  //else callback(newKey);
+                  console.log('keygen', newKey);
+                  callback(newKey);
+              });
+            }
+
            
+           /*
            col.find({'url':url}).toArray(function(err, data){
+             
             if(err) throw err;
-            
+           
             //if url does not exist in database
             if(data.length){
-                console.log('found', data[0]['key']);
+                //console.log('found', data[0]['key']);
                 obj["short_url"] = baseUrl + data[0]['key'];
             }else{
                 code = codeGen(digit, '');
@@ -53,15 +99,16 @@ app.get(/short\/(.+)/, function(req, res){
                    console.log("code_part1", code);
                    obj["short_url"] = baseUrl + code;
                    col.insert({key:code, url:url});
+                   //insertData(col, code, url);
                }
                
             }
             
             return res.json(obj);
             
-            });
+            });*/
             
-        });
+    });
    
     
   
@@ -83,13 +130,6 @@ app.get('/:short_url', function(req, res){
             //var thisUrl;
             if(err) throw err;
             
-            /*
-            data.forEach(function(item){
-                if(item['key'] && Object.keys(item['key'])[0] === short_url){
-                    thisUrl = item['key'][Object.keys(item['key'])[0]];
-                   return;
-                }
-            });*/
             
             if(!data) {
                 resObj.error = "the url does not exist";
@@ -106,22 +146,19 @@ app.get('/:short_url', function(req, res){
 
 app.listen(process.env.PORT || 8080, function(){});
 
-function accessDb(key, callback){
+function accessDb(callback){
     
     mongo.connect(url_db, function(err, db){
        if(err) throw err;
-       
-       var col = db.collection('codebase');
-       
-       callback(null, col);
-       
-       db.close();
+       var col = db.collection('urls');
+       callback(null, db, col);
+       //db.close();
     });
 }
-
 
 function codeGen(digit, code){
     if(digit<1) return code;
     code += pool[Math.floor(Math.random()*pool.length)];
     return codeGen(digit-1, code);
 }
+
